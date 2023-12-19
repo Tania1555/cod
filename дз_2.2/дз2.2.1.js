09f1ddef5b5d2bd3b2f0c2a051af70728ea2f9af
Tania1555 <141180903+Tania1555@users.noreply.github.com>
2023-12-19 23:07:53 +0400

    Add files via upload

---
+++ дз_2.2/дз2.2.1.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 2;
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+let c = 10;
+let d = 5;
+let result = c + d;
+console.log(result);
+a = 1.5;
+b = 0.75;
+console.log(a + b);
+a = 1;
+console.log(-a);
+s1 = "Усова"
+s2 = "Татьяна"
+console.log(s1, s2)
---
+++ дз_2.2/дз2.2.2.js
@@ -0,0 +1,29 @@
+let myName = "Таня";
+console.log(myName);
+let x = 1;
+let y = 2;
+console.log(x + y);
+let isAdult = false;
+console.log(isAdult);
+let text = "HelloWord";
+console.log(text.length);
+let pi = Math.PI;
+console.log(pi);
+let isAdmin = true;
+console.log(isAdmin);
+let width = 10;
+let height = 5;
+console.log(width * height);
+let today = "19.12.2023";
+console.log(today);
+let isPositive;
+if (x > 0) {
+    isPositive = true;
+}
+else {
+    isPositive = false;
+};
+console.log(isPositive);
+let a = +prompt("Введите первое число", "");
+let b = +prompt("Введите второе число", "");
+console.log(a + b);
