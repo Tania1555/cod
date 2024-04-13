460442b1458d6a70ab0200485125b327fa64375b
Tania1555 <141180903+Tania1555@users.noreply.github.com>
2024-04-13 13:46:55 +0400

    Add files via upload

---
+++ дз_4.1/App1.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <button @click="addUser('User ', 'This is User ')">Добавить элемент</button>
+    <hr>
+    <ul>
+      <li class="users" v-for="user in users" :key="user" @click="showUser(user)">{{ user }}</li>
+    </ul>
+    <hr>
+    <p>{{ suser }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      number: 3,
+      users: ['User 1. This is User 1', 'User 2. This is User 2', 'User 3. This is User 3'],
+      suser: ''
+    }
+  },
+  methods: {
+    addUser(user, information) {
+      this.number += 1
+      this.users.push(user + String(this.number) + '. ' + information + String(this.number))
+      
+    },
+    showUser(user) {
+      this.suser = user + '. ' + 'He is in the list of users.'
+    }
+  }
+}
+</script>
+
+<style>
+  li {
+    padding: 5px;
+    flex-basis: 100%;
+  }
+  div {
+    font-size: 25px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: center;
+  }
+  p {
+    text-align: center;
+    flex-basis: 100%;
+  }
+  button {
+    flex-basis: 30%;
+    font-size: 20px;
+    margin-top: 3%;
+  }
+  hr {
+    flex-basis: 80%;
+  }
+</style>
---
+++ дз_4.1/App2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p @click="changeText">{{ text1 }}</p>
+    <p>{{ text2 }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyText',
+  data() {
+    return {
+      text1: 'Hello',
+      text2: 'World',
+    }
+  },
+  methods: {
+    changeText() {
+      this.text1 = 'Goodbye'
+    }
+  }
+}
+</script>
+<style>
+  div {
+    display: flex;
+    justify-content: center;
+    font-size: 65px;
+    margin-top: 5%;
+  }
+  p {
+    padding: 3%;
+  }
+</style>
