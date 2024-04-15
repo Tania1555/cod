0b6541303507c79901e2f95b41b88f628ce76344
Tania1555 <141180903+Tania1555@users.noreply.github.com>
2024-04-15 23:17:42 +0400

    Add files via upload

---
+++ дз_4.2/App1.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <hr>
+    <ul>
+      <li class="users" v-for="user in users" :key="user.id">{{ user.name }}, Age: {{ user.age }}, {{ user.administrator }}</li>
+    </ul>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyComponent',
+  data() {
+    return {
+      users: [{id: 1, name:"Tania", age: 14, administrator: "She is the administrator"}, {id: 2, name:"Vova", age: 10, administrator: "He isn't the administrator"}, {id: 3, name:"Slava", age: 17, administrator: "He isn't the administrator"}],
+    }
+  },
+}
+</script>
+
+<style>
+  li {
+    padding: 5px;
+    flex-basis: 100%;
+  }
+  div {
+    font-size: 25px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: center;
+  }
+  p {
+    text-align: center;
+    flex-basis: 100%;
+  }
+  button {
+    flex-basis: 30%;
+    font-size: 20px;
+    margin-top: 3%;
+  }
+  hr {
+    flex-basis: 80%;
+  }
+</style>
---
+++ дз_4.2/App2.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="content">
+    <div class="a">
+    <button @click="deleteExample">Удалить задачу</button>
+    </div>
+    <ul>
+      <li class="examples" v-for="example in examples" :key="example.id">{{ example.title }}. {{ example.description }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskList',
+  data() {
+    return {
+      examples: [
+{ id: 1, title: "Задача 1", description: "Сколько будет 2 + 3?" },
+{ id: 2, title: "Задача 2", description: "Сколько будет 2 * 3?" },
+{ id: 3, title: "Задача 3", description: "Сколько будет 2 - 3?" }
+],
+    }
+  },
+  methods: {
+    deleteExample() {
+      this.examples.pop()
+    }
+  }
+}
+</script>
+
+<style>
+  li {
+    padding: 5px;
+    flex-basis: 100%;
+  }
+  .content {
+    font-size: 25px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    justify-content: center;
+  }
+  button {
+    width: 30%;
+    font-size: 20px;
+    margin-top: 3%;
+  }
+  .a {
+    flex-basis: 100%;
+    display: flex;
+    justify-content: center;
+  }
+</style>
