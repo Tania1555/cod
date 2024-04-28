abc10452621e37481ada537c6deccfed70607e99
Tania1555 <141180903+Tania1555@users.noreply.github.com>
2024-04-28 16:23:14 +0400

    Add files via upload

---
+++ дз_4.4/App1.vue
@@ -0,0 +1,90 @@
+<template>
+  <h1>Выбери себе котокуба</h1>
+  <div class="content">
+    <div class="items">
+      <div><img src="https://basket-12.wbbasket.ru/vol1754/part175416/175416028/images/big/2.webp"></div><br>
+      <p>Трёхцветный (приносит удачу, редкий)</p><div class="price"><label>1500 руб. </label><input type="checkbox" v-model="array" value=1500></div>
+    </div>
+    <div class="items">
+      <div><img src="https://basket-12.wbbasket.ru/vol1689/part168948/168948575/images/big/1.jpg"></div><br>
+      <p>Рыжий (приносит счастье)</p><div class="price"><label>1300 руб. </label><input type="checkbox" v-model="array" value=1300></div>
+    </div>
+    <div class="items">
+      <div><img src="https://basket-10.wbbasket.ru/vol1535/part153596/153596019/images/big/1.jpg"></div><br>
+      <p>Черный (приносит богатство)</p><div class="price"><label>1350 руб. </label><input type="checkbox" v-model="array" value=1350></div>
+    </div>
+  </div>   
+  <div class="text">Итого: {{ sumArray(array) }} руб.</div>
+  <span v-if="!sumArray(array)==0">Прекрасный выбор!</span>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      array: []
+    }
+  },
+  methods: {
+    sumArray(arr){
+      return arr.reduce((total, num) => total + Number(num), 0)
+    }
+  }
+}
+</script>
+<style>
+img {
+  height: 55vh;
+  flex-basis: 100%
+}
+.content {
+  display: flex;
+  justify-content: space-around;
+}
+.items {
+  flex-basis: 30%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding-top: 3vh;
+  padding-bottom: 3vh;
+  border: 3px solid rgba(122, 122, 251, 0.499);
+  border-radius: 30px;
+}
+label {
+  font-size: 3.5vh;
+  font-family: Comic Sans MS, Comic Sans, cursive;
+}
+p {
+  flex-basis: 100%;
+  text-align: center;
+  font-family: Comic Sans MS, Comic Sans, cursive;
+  font-size: 3vh;
+}
+input[type=checkbox] {
+    transform: scale(1.5);
+}
+.price {
+  background-color: rgba(159, 159, 220, 0.39);
+  padding: 0.5vh;
+  border: 2px solid rgba(122, 122, 251, 0.689);
+  border-radius: 10px;
+}
+h1 {
+  font-family: Comic Sans MS, Comic Sans, cursive;
+  text-align: center;
+  font-size: 6vh;
+}
+.text {
+  font-family: Comic Sans MS, Comic Sans, cursive;
+  text-align: center;
+  font-size: 5vh;
+  margin: 3vh;
+}
+span {
+  font-family: Comic Sans MS, Comic Sans, cursive;
+  text-align: center;
+  font-size: 4vh;
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ дз_4.4/App2.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="content">
+    <div class="mincontent">
+    <h1 class="items">Заполни форму</h1>
+    <div class="items"><input v-model="inputValue1" type="text" placeholder="Имя"></div>
+    <div class="items"><input v-model="inputValue2" type="email" placeholder="Email"></div>
+    <div class="items"><textarea v-model="text" placeholder="Сообщение"></textarea></div>
+    <div class="items"><button @click="addMessage(inputValue1, inputValue2, text)" :disabled="inputValue1 == '' || inputValue2 == '' || text == ''">Отправить</button></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        number: 1,
+        messages: [],
+        inputValue1: '',
+        inputValue2: '',
+        text: ''
+      }
+    },
+    methods: {
+      addMessage(n, e, m) {
+        this.messages.push({id: this.number, name: n, email: e, message: m}),
+        this.number += 1,
+        this.inputValue1 = '',
+        this.inputValue2 = '',
+        this.text = '',
+        console.log(this.messages)
+      }
+    }
+  }
+</script>
+
+<style>
+  .content {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    height: 100vh;
+    align-content: center;
+    flex-basis: 100%;
+  }
+  .items {
+    flex-basis: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 2vh;
+  }
+  input {
+    height: 5vh;
+    width: 30vw;
+    font-size: 3vh;
+    border: 0.5vh solid rgba(144, 221, 189, 0.557);
+  }
+  textarea {
+    resize: vertical;
+    width: 30vw;
+    height: 10vh;
+    font-size: 3vh;
+    border: 0.5vh solid rgba(144, 221, 189, 0.557);
+  }
+  .mincontent {
+    padding: 4vh;
+    border: 0.5vh solid rgba(144, 221, 189, 0.557);
+    border-radius: 5vh;
+  }
+  button {
+    width: 10vw;
+    height: 5vh;
+    font-size: 3vh;
+    border: 0.5vh solid rgba(144, 221, 189, 0.557);
+    background-color: rgba(144, 221, 189, 0.383);
+  }
+  h1 {
+    font-size: 6vh;
+    color: rgba(61, 121, 96, 0.921);
+  }
+</style>
